302404b6f747177a43b5a5a2518f732bd3b13229
Tesla <bolarinwaahmed22@gmail.com>
2022-06-15 15:45:58 +0100

    Added banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Image from "../utils/Images";
 import style from "./../css/banner.module.css";
 const Banner = () => {
   return (
-    <section>
-      <div className={style.text}>
-        <h1>Hope To Better Relationship & Lifestyle</h1>
+    <section className={style.banner}>
+      <div className={style.banner__content}>
+        <div className={style.text}>
+          <h1>Hope To Better Relationship & Lifestyle</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            malesuada elit quis arcu sollicitudin, eu bibendum urna tristique.{" "}
+          </p>
+          <button>Get Started</button>
+        </div>
+        <div className={style.image}>
+          <img src={Image.bannerImage} alt="" />
+        </div>
       </div>
-      <div className={style.image}></div>
     </section>
   );
 };
--- src/utils/Images.js
+++ src/utils/Images.js
@@ -1,7 +1,9 @@
 import logo from "./../images/logo.png";
+import bannerImage from "./../images/bannerImage.png";
 
 const Image = {
   logo,
+  bannerImage,
 };
 
 export default Image;
