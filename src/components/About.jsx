c8d73746dc05b3ffd978480164ca348d8240e517
Tesla <bolarinwaahmed22@gmail.com>
2022-06-23 14:44:29 +0100

    Created the about section clip-path

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,22 +4,25 @@ import style from "./../css/about.module.css";
 
 const About = () => {
   return (
-    <section className={style.about}>
-      <div className={style.about__text}>
-        <h4>About Us</h4>
-        <p>
-          Morbi laoreet nisl malesuada nisi condimentum volutpat. Vestibulum
-          imperdiet metus ac neque malesuada viverra non non turpis. <br />{" "}
-          <br />
-          Aliquam sit amet mollis dolor, finibus porta urna. Nulla facilisi.
-          Vivamus laoreet risus in libero pellentesque, at imperdiet nunc
-          tincidunt. Vestibulum
-        </p>
-        <button>Read More.</button>
+    <section>
+      <div className={style.about}>
+        <div className={style.about__text}>
+          <h4>About Us</h4>
+          <p>
+            Morbi laoreet nisl malesuada nisi condimentum volutpat. Vestibulum
+            imperdiet metus ac neque malesuada viverra non non turpis. <br />{" "}
+            <br />
+            Aliquam sit amet mollis dolor, finibus porta urna. Nulla facilisi.
+            Vivamus laoreet risus in libero pellentesque, at imperdiet nunc
+            tincidunt. Vestibulum
+          </p>
+          <button>Read More.</button>
+        </div>
+        <div className={style.about__image}>
+          <img src={Image.aboutImage} alt="" />
+        </div>
       </div>
-      <div className={style.about__image}>
-        <img src={Image.aboutImage} alt="" />
-      </div>
+      <div className={style.about__foot}></div>
     </section>
   );
 };
